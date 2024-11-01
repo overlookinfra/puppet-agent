93e6c0b7efa6ba6ff46f47d9ccbaa720eb5de474
nmburgan <13688219+nmburgan@users.noreply.github.com>
2024-11-01 10:38:05 -0700

    Fixes for containerized builds You'll need to point to your own pxp-agent and puppet-runtime build.

--- configs/platforms/el-7-x86_64.rb
+++ configs/platforms/el-7-x86_64.rb
@@ -1,10 +1,3 @@
 platform "el-7-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  packages = ['make', 'rsync', 'rpm-build']
-  plat.provision_with "yum install --assumeyes #{packages.join(' ')}"
-  plat.install_build_dependencies_with "yum install --assumeyes"
-  plat.vmpooler_template "centos-7-x86_64"
+  plat.inherit_from_default
 end
--- configs/platforms/sles-15-x86_64.rb
+++ configs/platforms/sles-15-x86_64.rb
@@ -1,10 +1,3 @@
 platform "sles-15-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  packages = ['make', 'rsync', 'rpm-build']
-  plat.provision_with "zypper -n --no-gpg-checks install -y #{packages.join(' ')}"
-  plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
-  plat.vmpooler_template "sles-15-x86_64"
+  plat.inherit_from_default
 end
