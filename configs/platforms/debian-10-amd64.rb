161db24f7e086f07d12f7bf58e475d0b2a7177bc
nmburgan <13688219+nmburgan@users.noreply.github.com>
2024-12-18 12:07:21 -0800

    8.10.0-overlookinfra

--- configs/platforms/debian-10-amd64.rb
+++ configs/platforms/debian-10-amd64.rb
@@ -1,11 +1,3 @@
 platform "debian-10-amd64" do |plat|
-  plat.servicedir "/lib/systemd/system"
-  plat.defaultdir "/etc/default"
-  plat.servicetype "systemd"
-  plat.codename "buster"
-
-  packages = ['build-essential', 'devscripts', 'rsync', 'fakeroot', 'debhelper']
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
-  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
-  plat.vmpooler_template "debian-10-x86_64"
+  plat.inherit_from_default
 end
--- configs/platforms/el-8-ppc64le.rb
+++ configs/platforms/el-8-ppc64le.rb
@@ -1,14 +1,3 @@
 platform "el-8-ppc64le" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
-
-  # Workaround for an issue with RedHat subscription metadata, see ITSYS-2543
-  plat.provision_with('subscription-manager repos --disable rhel-8-for-ppc64le-baseos-rpms && subscription-manager repos --enable rhel-8-for-ppc64le-baseos-rpms')
-
-  packages = %w(gcc gcc-c++ autoconf automake createrepo rsync cmake make rpm-libs rpm-build libarchive)
-
-  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
-  plat.install_build_dependencies_with "dnf install -y --allowerasing "
-  plat.vmpooler_template "redhat-8-power8"
+  plat.inherit_from_default
 end
